590c1e670ce74e7e57911b102764c37eaea89536
deividstaehr <deividstaehr@hotmail.com>
2020-01-23 16:18:02 -0300

    início

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import codePush from 'react-native-code-push';
+
+function Index() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>Teste Deploy</Text>
+    </View>
+  );
+}
+
+export default codePush({
+    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#333',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 20,
+    },
+});
